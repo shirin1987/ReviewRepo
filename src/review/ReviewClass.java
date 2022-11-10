ba49afad1bdbf54e7be9411ef7bce2fa763262a3
shirin1987 <117659289+shirin1987@users.noreply.github.com>
2022-11-10 16:12:54 -0500

    Update ReviewClass.java
    Main method

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -2,12 +2,5 @@ package review;
 
 public class ReviewClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	public void test() {
-		System.out.println("This method is on Review Session Branch ");
-	}
 
 }
